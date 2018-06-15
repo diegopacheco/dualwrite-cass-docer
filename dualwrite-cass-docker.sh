5290b0c8be5506b3ec50a6251043d73dae5ee585
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-15 12:49:58 -0700

    added schema support

--- dualwrite-cass-docker.sh
+++ dualwrite-cass-docker.sh
@@ -49,6 +49,22 @@ function status3x(){
   docker exec -ti cassandra3x_3 /cassandra/bin/nodetool status
 }
 
+function schema(){
+  docker exec -ti cassandra2x_1 sh -c "echo \"
+   CREATE KEYSPACE CLUSTER_TEST WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
+   USE CLUSTER_TEST;
+   CREATE TABLE TEST ( key text PRIMARY KEY, value text);
+   INSERT INTO TEST (key,value) VALUES ('1', 'works');
+   SELECT * from CLUSTER_TEST.TEST;\" | /cassandra/bin/cqlsh 128.18.0.21"
+
+  docker exec -ti cassandra3x_1 sh -c "echo \"
+    CREATE KEYSPACE CLUSTER_TEST WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
+    USE CLUSTER_TEST;
+    CREATE TABLE TEST ( key text PRIMARY KEY, value text);
+    INSERT INTO TEST (key,value) VALUES ('1', 'works');
+    SELECT * from CLUSTER_TEST.TEST;\" | /cassandra/bin/cqlsh 128.18.0.31"
+}
+
 function help(){
   echo "DualWrite-Cass-Docker: by Diego Pacheco"
   echo "bake                 : bake the docker image for Cass 2.x and 3.x "
@@ -58,6 +74,7 @@ function help(){
   echo "status3x             : nodetool status in all cass 3x nodes       "
   echo "cql2x                : cqlsh in first cass 3x node                "
   echo "cql3x                : cqlsh in first cass 2x node                "
+  echo "schema               : create same schema for cass 2x and 3x      "
 }
 
 case $1 in
@@ -81,6 +98,9 @@ case $1 in
           ;;
       "cql3x")
           cql3x
+          ;;
+      "schema")
+          schema
           ;;
       *)
           help
